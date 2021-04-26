4eda45b15d67c72bc2612144cad2c3d4deae0444
Asus <Asus@DESKTOP-7QSBH7L>
2021-04-26 22:58:35 +0300

    componentler modüllere ayrıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http'
-
+import { HttpClientModule } from '@angular/common/http';
+import { MainModule } from './pages/main.module';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ContactComponent } from './contact/contact.component';
-import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
-import { MainNavComponent } from './nav/main-nav/main-nav.component';
+
 import { AdminNavComponent } from './nav/admin-nav/admin-nav.component';
-import { MenuCategoryComponent } from './components/menu-category/menu-category.component';
-
-
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutMeComponent,
-    ContactComponent,
-    MainLayoutComponent,
-    AdminLayoutComponent,
-    MainNavComponent,
-    AdminNavComponent,
-    MenuCategoryComponent,
-
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, AdminLayoutComponent, AdminNavComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, MainModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
