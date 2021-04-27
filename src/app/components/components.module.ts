3ef99e9a2e0fcfd8bf67a86d55b7deb73411958e
Asus <Asus@DESKTOP-7QSBH7L>
2021-04-27 23:29:23 +0300

    page title component geliştirmesi

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { MenuCategoryComponent } from './menu-category/menu-category.component';
+import { PageTitleComponent } from './page-title/page-title.component';
 
 @NgModule({
-  declarations: [MenuCategoryComponent],
+  declarations: [MenuCategoryComponent, PageTitleComponent],
   imports: [CommonModule, RouterModule],
-  exports: [MenuCategoryComponent],
+  exports: [MenuCategoryComponent,PageTitleComponent],
 })
 export class ComponentsModule {}
